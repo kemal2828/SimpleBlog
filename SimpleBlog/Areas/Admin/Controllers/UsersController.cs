2f41f4a21f880b4243a7feb3d42d9c136f6aa258
Kemal_hp\Kemal <KEMAL.y93>
2017-03-07 17:46:42 +0300

    inilitaire commit

---
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Admin/Users/
+        public ActionResult Index()
+        {
+            return Content("Admin area users controller index action");
+        }
+        public ActionResult List()
+        {
+            return Content("Admin area users controller list action");
+        }
+        public ActionResult Edit(int id = 0)
+        {
+            return Content("Admin area users controller edit action"+id);
+        }
+	}
+}
